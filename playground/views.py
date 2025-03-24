9bb61f726c84c7e0690bd23bbd17cde00f620621
Zdravko <zdravko.galchov@gmail.com>
2025-03-24 23:54:11 +0000

    QuerySet exercises

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 def say_hello(request):
-    a = 5
-    b = 10
-    return render(request, 'hello.html', {'name': 'Raymond'})
+    # Customers with '.com' ending emails
+    # queryset = Customer.objects.filter(email__icontains='.com')
+
+    # Collections that don't have a featured product
+    # queryset = Collection.objects.filter(featured_product__isnull=True)
+
+    # Products with low inventory
+    # queryset = Product.objects.filter(inventory__lt=10)
+
+    # Order, placed by customer with id = 1
+    # queryset = Order.objects.filter(customer__id=1)
+
+    # Order items for products in collection 3
+    queryset = OrderItem.objects.filter(product__collection__id=3)
+
+    return render(request, 'hello.html', {'name': 'Raymond', 'products': list(queryset)})
